36dc5eb8b7d70a97e2c9eeb793156a8278a6bfa7
Maima Ismail <maimamirza@gmail.com>
2023-07-03 16:42:02 -0700

    first commit

---
+++ src/CButton.jsx
@@ -0,0 +1,10 @@
+const CButton = ({ id, className, onClick, value }) => {
+  return (
+    <div>
+      <button id={id} className={className} onClick={onClick}>
+        {value}
+      </button>
+    </div>
+  )
+}
+export default CButton
