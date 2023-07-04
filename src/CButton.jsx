ca1ea2d3759856ac09e55260723bfca314674006
Shahrukh <awaisfazli947@gmail.com>
2023-07-04 11:01:28 +0500

    Fixed the button styling

--- src/CButton.jsx
+++ src/CButton.jsx
@@ -1,10 +1,10 @@
 const CButton = ({ id, className, onClick, value }) => {
   return (
-    <div>
-      <button id={id} className={className} onClick={onClick}>
-        {value}
-      </button>
-    </div>
-  )
-}
-export default CButton
+    // <div>
+    <button id={id} className={className} onClick={onClick}>
+      {value}
+    </button>
+    // </div>
+  );
+};
+export default CButton;
